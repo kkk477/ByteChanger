ea4bd2378cc7a5097779e627a4099620e8aedf37
shhan.ictway <shhan.ictway@gmail.com>
2023-02-21 11:32:09 +0900

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.insertCharText = new System.Windows.Forms.TextBox();
+            this.deleteCharText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFileDialog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // insertCharText
+            // 
+            this.insertCharText.Location = new System.Drawing.Point(163, 40);
+            this.insertCharText.Name = "insertCharText";
+            this.insertCharText.PlaceholderText = "A,12";
+            this.insertCharText.Size = new System.Drawing.Size(318, 27);
+            this.insertCharText.TabIndex = 0;
+            // 
+            // deleteCharText
+            // 
+            this.deleteCharText.Location = new System.Drawing.Point(163, 104);
+            this.deleteCharText.Name = "deleteCharText";
+            this.deleteCharText.PlaceholderText = "3";
+            this.deleteCharText.Size = new System.Drawing.Size(318, 27);
+            this.deleteCharText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "입력할 문자와 위치";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "삭제할 위치";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(506, 101);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 33);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "작업 시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_ClickAsync);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenFileDialog
+            // 
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(506, 37);
+            this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(124, 33);
+            this.btnOpenFileDialog.TabIndex = 5;
+            this.btnOpenFileDialog.Text = "파일";
+            this.btnOpenFileDialog.UseVisualStyleBackColor = true;
+            this.btnOpenFileDialog.Click += new System.EventHandler(this.btnOpenFileDialog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 188);
+            this.Controls.Add(this.btnOpenFileDialog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deleteCharText);
+            this.Controls.Add(this.insertCharText);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "바이트 일괄 수정 프로그램";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox insertCharText;
+        private System.Windows.Forms.TextBox deleteCharText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<string> fileList;
+        string insertText;
+        int insertPosition;
+        int deletePosition;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            fileList.Clear();
+            
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Multiselect = true;
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                fileList = openFileDialog1.FileNames.ToList();
+            }
+
+            if (fileList.Count > 0)
+                MessageBox.Show("파일 선택 완료");
+        }
+
+        private async void btnStart_ClickAsync(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(insertCharText.Text) && String.IsNullOrEmpty(deleteCharText.Text))
+            {
+                MessageBox.Show("작업을 아무것도 선택하지 않았습니다.");
+                return;
+            }
+
+            if (fileList.Count == 0)
+            {
+                MessageBox.Show("작업을 아무것도 선택하지 않았습니다.");
+                return;
+            }
+
+            if (!String.IsNullOrEmpty(insertCharText.Text))
+            {
+                string[] parse = insertCharText.Text.Split(",");
+                insertText = parse[0];
+                insertPosition = Convert.ToInt32(parse[1]);
+
+                await InsertCharAsync(insertText, insertPosition);
+            }
+
+            if (!String.IsNullOrEmpty(deleteCharText.Text))
+            {
+                deletePosition = Convert.ToInt32(deleteCharText.Text);
+                await DeleteCharAsync(deletePosition);
+            }
+
+            MessageBox.Show("작업 완료!!!");
+        }
+
+        private Task InsertCharAsync(string character, int position)
+        {
+            return Task.Run(() => {
+                foreach (var file in fileList)
+                {
+
+                    var reader = File.ReadAllBytes(file);
+                    byte[] data = Encoding.ASCII.GetBytes(character);
+                    var a = reader.ToList();
+                    a.Insert(position, data[0]);
+                    File.WriteAllBytes(file, a.ToArray());
+                }
+            });
+        }
+
+        private Task DeleteCharAsync(int position)
+        {
+            return Task.Run(() => {
+                foreach (var file in fileList)
+                {
+                    var reader = File.ReadAllBytes(file);
+                    var a = reader.ToList();
+                    a.RemoveAt(position);
+                    File.WriteAllBytes(file, a.ToArray());
+                }
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fileList = new List<string>();
+        }
+    }
+}
